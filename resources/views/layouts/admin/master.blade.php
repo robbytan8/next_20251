f4e6105c73181ae68025edf9ecb3347ce89abd0a
Robby Tan <robby-tan@outlook.com>
2026-04-09 00:15:54 +0700

    Update master to use CDNs
    Replace several local assets with CDN links and tidy head formatting. Remove the preloader markup and extra HTML comment.

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -1,62 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-<!-- [Head] start -->
 
 <head>
   <title>My Admin Template</title>
-  <!-- [Meta] -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="description" content="Mantis template form Project NEXT 2025.">
-  <meta name="keywords" content="Mantis, Dashboard UI Kit, Bootstrap 5, Admin Template, Admin Dashboard, CRM, CMS, Bootstrap Admin Template">
+  <meta name="keywords"
+    content="Mantis, Dashboard UI Kit, Bootstrap 5, Admin Template, Admin Dashboard, CRM, CMS, Bootstrap Admin Template">
   <meta name="author" content="Robby Tan">
 
   <!-- [Favicon] icon -->
-  <link rel="icon" href="{{ asset('assets/images/favicon.svg') }}" type="image/x-icon"> <!-- [Google Font] Family -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap" id="main-font-link">
+  <link rel="icon" href="{{ asset('assets/images/favicon.svg') }}" type="image/x-icon">
+  <!-- [Google Font] Family -->
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap"
+    id="main-font-link">
   <!-- [Tabler Icons] https://tablericons.com -->
-  <link rel="stylesheet" href="{{ asset('assets/fonts/tabler-icons.min.css') }}">
+  <link href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css" rel="stylesheet">
   <!-- [Feather Icons] https://feathericons.com -->
-  <link rel="stylesheet" href="{{ asset('assets/fonts/feather.css') }}">
+  <link href="{{ asset('assets/fonts/feather.css') }}" rel="stylesheet">
   <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
-  <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome.css') }}">
-  <!-- [Material Icons] https://fonts.google.com/icons -->
-  <link rel="stylesheet" href="{{ asset('assets/fonts/material.css') }}">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+  <!-- [Bootstrap Icons] https://icons.getbootstrap.com -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <!-- [Template CSS Files] -->
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" id="main-style-link">
-  <link rel="stylesheet" href="{{ asset('assets/css/style-preset.css') }}" id="preset-style-link">
+
   @yield('CSS')
 </head>
-<!-- [Head] end -->
-<!-- [Body] Start -->
 
 <body data-pc-preset="preset-1" data-pc-direction="ltr" data-pc-theme="light">
-  <!-- [ Pre-loader ] start -->
-  <div class="loader-bg">
-    <div class="loader-track">
-      <div class="loader-fill"></div>
-    </div>
-  </div>
-  <!-- [ Pre-loader ] End -->
-  <!-- [ Sidebar Menu ] start -->
+  
   @include('layouts.admin.sidebar')
-  <!-- [ Sidebar Menu ] end --> <!-- [ Header Topbar ] start -->
+  
   @include('layouts.admin.header')
-  <!-- [ Header ] end -->
-
-  <!-- [ Main Content ] start -->
+  
   @yield('content')
-  <!-- [ Main Content ] end -->
+  
   @include('layouts.admin.footer')
 
   <!-- Required Js -->
-  <script src="{{ asset('assets/js/plugins/popper.min.js') }}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
   <script src="{{ asset('assets/js/plugins/simplebar.min.js') }}"></script>
-  <script src="{{ asset('assets/js/plugins/bootstrap.min.js') }}"></script>
-  <script src="{{ asset('assets/js/fonts/custom-font.js') }}"></script>
   <script src="{{ asset('assets/js/pcoded.js') }}"></script>
-  <script src="{{ asset('assets/js/plugins/feather.min.js') }}"></script>
   @yield('JS')
   <script>
     change_box_container('false');
@@ -72,5 +62,4 @@
   </script>
 
 </body>
-<!-- [Body] end -->
 </html>
